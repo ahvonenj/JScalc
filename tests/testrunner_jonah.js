f771c10772d3a0eb1a7e46fbf5a4b63199ddb352
ahvonenj <jonah.ahvonen@gmail.com>
2015-03-26 13:41:56 +0200

    ???

--- tests/testrunner_jonah.js
+++ tests/testrunner_jonah.js
@@ -44,7 +44,7 @@ testrunner.setup(
 
 testrunner.run(
 {
-	code: {path: "/var/lib/jenkins/jobs/JonahJob/workspace/tests/calculator.testable.js", namespace: "calculator"},
+	code: {path: "/var/lib/jenkins/jobs/JonahJob/workspace/tests/calculator_testable.js", namespace: "calculator"},
     tests: "/var/lib/jenkins/jobs/JonahJob/workspace/tests/tests_jonah.js"
 }, function(err, report) 
 {
