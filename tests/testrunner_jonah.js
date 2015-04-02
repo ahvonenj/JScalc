6dfe6d169cdf9904d83aa84a37ca4f7e918a88b1
J <jonah.ahvonen@gmail.com>
2015-04-02 17:16:30 +0300

    jquery dep

--- tests/testrunner_jonah.js
+++ tests/testrunner_jonah.js
@@ -46,6 +46,7 @@ testrunner.setup(
 
 testrunner.run(
 {
+	deps: 'jquery',
 	code: "/var/lib/jenkins/jobs/JonahJob/workspace/js/calculator.js",
     tests: "/var/lib/jenkins/jobs/JonahJob/workspace/tests/tests_jonah.js"
 }, function(err, report) 
